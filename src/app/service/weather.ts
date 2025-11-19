c58b1abef8603f654622b1752392906cf6190678
Byron1235 <dazedkun@gmail.com>
2025-11-19 19:17:26 -0300

    ver1.10041 tweak

--- src/app/service/weather.ts
+++ src/app/service/weather.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class Weather {
 
-  private apiKey = 'api/weather';
-  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiUrl = '/api/weather';
 
   constructor(private http: HttpClient) {}
 
   getWeather(city: string) {
-    return this.http.get(`${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric&lang=en`);
+    return this.http.get(`${this.apiUrl}?city=${city}`);
   }
 }
+
